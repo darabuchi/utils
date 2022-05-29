929be6f817dace05b9b233ba702b058b7e014518
落心 <luoxin.ttt@gmail.com>
2022-05-29 14:01:24 +0800

    font

--- table/font.go
+++ table/font.go
@@ -1,8 +1,6 @@
 package table
 
 import (
-	"embed"
-	_ "embed"
 	"image"
 	"unicode"
 	
@@ -13,48 +11,26 @@ import (
 	"golang.org/x/image/font"
 )
 
-//go:embed font
-var fontFs embed.FS
-
 var fontList []*truetype.Font
 
 const (
 	defaultFontSize = 18
 )
 
-func init() {
-	add := func(path string) {
-		log.Infof("load font %s", path)
-		fontBuf, err := fontFs.ReadFile("font/" + path)
-		if err != nil {
-			log.Panicf("err:%v", err)
-			return
-		}
-		f, err := freetype.ParseFont(fontBuf)
-		if err != nil {
-			log.Panicf("err:%v", err)
-			return
-		}
-		fontList = append(fontList, f)
-	}
-	
-	dirs, err := fontFs.ReadDir("font")
-	if err != nil {
-		log.Panicf("err:%v", err)
-		return
-	}
-	
-	for _, dir := range dirs {
-		if dir.IsDir() {
-			continue
-		}
-		add(dir.Name())
-	}
+func AddFount(f *truetype.Font) {
+	fontList = append(fontList, f)
 }
 
-// func SetFont(f *truetype.Font) {
-// 	_font = f
-// }
+func AddFontWithBuf(fontBuf []byte) error {
+	f, err := freetype.ParseFont(fontBuf)
+	if err != nil {
+		log.Panicf("err:%v", err)
+		return err
+	}
+	
+	AddFount(f)
+	return nil
+}
 
 func TextSize(label string, fontSize float64) *Size {
 	return NewSize(fontSize*2*FontLen(label), (fontSize+float64(int64(fontSize)>>6))*2)
@@ -73,15 +49,17 @@ func FontLen(str string) float64 {
 }
 
 func DrawFont(dst *image.RGBA, src image.Image, x, y float64, label string, fontSize float64) {
-	face := new(multiface.Face)
-	for _, f := range fontList {
-		face.AddTruetypeFace(truetype.NewFace(f, &truetype.Options{Size: fontSize, DPI: 144}), f)
-	}
 	
 	d := font.Drawer{}
 	d.Dst = dst
 	d.Src = src
+	
+	face := new(multiface.Face)
+	for _, f := range fontList {
+		face.AddTruetypeFace(truetype.NewFace(f, &truetype.Options{Size: fontSize, DPI: 144}), f)
+	}
 	d.Face = face
+	
 	d.Dot = freetype.Pt(int(x), int(y+fontSize*2))
 	d.DrawString(label)
 }
