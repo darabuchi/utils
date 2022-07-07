b9e70a729ec7ef5df91cbf717efb1a50f080f85c
Luoxin <luoxin.ttt@gmail.com>
2022-07-07 11:56:42 +0800

    sea

---
+++ aes.go
@@ -0,0 +1,86 @@
+package utils
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+)
+
+func AESECBEncrypt(src []byte, key []byte) (encrypted []byte) {
+	cipher, _ := aes.NewCipher(generateKey(key))
+	length := (len(src) + aes.BlockSize) / aes.BlockSize
+	plain := make([]byte, length*aes.BlockSize)
+	copy(plain, src)
+	pad := byte(len(plain) - len(src))
+	for i := len(src); i < len(plain); i++ {
+		plain[i] = pad
+	}
+	encrypted = make([]byte, len(plain))
+	for bs, be := 0, cipher.BlockSize(); bs <= len(src); bs, be = bs+cipher.BlockSize(), be+cipher.BlockSize() {
+		cipher.Encrypt(encrypted[bs:be], plain[bs:be])
+	}
+	return encrypted
+}
+
+func AESECBDecrypt(encrypted []byte, key []byte) (decrypted []byte) {
+	cipher, _ := aes.NewCipher(generateKey(key))
+	decrypted = make([]byte, len(encrypted))
+	for bs, be := 0, cipher.BlockSize(); bs < len(encrypted); bs, be = bs+cipher.BlockSize(), be+cipher.BlockSize() {
+		cipher.Decrypt(decrypted[bs:be], encrypted[bs:be])
+	}
+	trim := 0
+	if len(decrypted) > 0 {
+		trim = len(decrypted) - int(decrypted[len(decrypted)-1])
+	}
+	return decrypted[:trim]
+}
+
+func generateKey(key []byte) (genKey []byte) {
+	genKey = make([]byte, 16)
+	copy(genKey, key)
+	for i := 16; i < len(key); {
+		for j := 0; j < 16 && i < len(key); j, i = j+1, i+1 {
+			genKey[j] ^= key[i]
+		}
+	}
+	return genKey
+}
+
+func AesCBCEncrypt(orig string, key string) string {
+	origData := []byte(orig)
+	k := []byte(key)
+	block, _ := aes.NewCipher(k)
+	blockSize := block.BlockSize()
+	origData = PKCS7Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, k[:blockSize])
+	cryted := make([]byte, len(origData))
+	blockMode.CryptBlocks(cryted, origData)
+	return base64.StdEncoding.EncodeToString(cryted)
+}
+
+func AesCBCDecrypt(cryted string, key string) string {
+	crytedByte, _ := base64.StdEncoding.DecodeString(cryted)
+	k := []byte(key)
+	block, _ := aes.NewCipher(k)
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, k[:blockSize])
+	orig := make([]byte, len(crytedByte))
+	blockMode.CryptBlocks(orig, crytedByte)
+	orig = PKCS7UnPadding(orig)
+	return string(orig)
+}
+
+// PKCS7Padding 补码
+func PKCS7Padding(ciphertext []byte, blocksize int) []byte {
+	padding := blocksize - len(ciphertext)%blocksize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+// PKCS7UnPadding 去码
+func PKCS7UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
---
+++ hmac.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"hash"
+)
+
+func hmacEncode(key, data string, h func() hash.Hash) string {
+	sha := hmac.New(h, []byte(key))
+	sha.Write([]byte(data))
+	return hex.EncodeToString(sha.Sum([]byte("")))
+}
+
+func HmacMd5(key, data string) string {
+	return hmacEncode(key, data, md5.New)
+}
+
+func HmacSha256(key, data string) string {
+	return hmacEncode(key, data, sha256.New)
+}
+
+func HmacSha224(key, data string) string {
+	return hmacEncode(key, data, sha256.New224)
+}
+
+func HmacSha512(key, data string) string {
+	return hmacEncode(key, data, sha512.New)
+}
+
+func HmacSha384(key, data string) string {
+	return hmacEncode(key, data, sha512.New384)
+}
