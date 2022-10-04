5efff050e20833d9002a684a72b81681a2b14a69
落心 <luoxin.ttt@gmail.com>
2022-10-04 14:57:56 +0800

    fix

---
+++ orm.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"reflect"
+
+	"github.com/pkg/errors"
+)
+
+func Scan(src interface{}, dst interface{}) error {
+	x := func(buf []byte) error {
+		bufLen := len(buf)
+		if bufLen >= 2 && buf[0] == '{' && buf[bufLen-1] == '}' {
+			return json.Unmarshal(buf, dst)
+		} else if bufLen > 0 {
+			return json.Unmarshal(buf, dst)
+		} else {
+			defaults.SetDefaults(dst)
+		}
+		return nil
+	}
+
+	switch r := src.(type) {
+	case []byte:
+		buf := src.([]byte)
+		return x(buf)
+	case string:
+		buf := []byte(src.(string))
+		return x(buf)
+	default:
+		return errors.New(
+			fmt.Sprintf("unknown type %v %s to scan", r, reflect.ValueOf(src).String()))
+	}
+}
+
+func Value(m interface{}) (driver.Value, error) {
+	if m == nil {
+		defaults.SetDefaults(m)
+	}
+
+	err := Validate(m)
+	if err != nil {
+		return nil, err
+	}
+
+	return json.Marshal(m)
+}
