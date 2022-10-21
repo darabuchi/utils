3b027e6d27448d569af6fc209ccc7418c703453d
Luoxin <luoxin.ttt@gmail.com>
2022-10-21 17:21:56 +0800

    fix

--- cache/statistics.go
+++ cache/statistics.go
@@ -10,8 +10,14 @@ import (
 
 func Statistics(key string, duration time.Duration) {
 	switch duration {
+	case xtime.Hour:
+		key = fmt.Sprintf("statistics:%s:hour:%s", key, time.Now().Format("2006010215"))
 	case xtime.Day:
 		key = fmt.Sprintf("statistics:%s:day:%s", key, time.Now().Format("20060102"))
+	// case xtime.Week:
+	// 	key = fmt.Sprintf("statistics:%s:weeb:%s", key, time.Now().Format("20060102"))
+	case xtime.Month:
+		key = fmt.Sprintf("statistics:%s:month:%s", key, time.Now().Format("200601"))
 	}
 
 	cnt, err := Incr(key)
@@ -22,8 +28,18 @@ func Statistics(key string, duration time.Duration) {
 
 	if cnt == 1 {
 		switch duration {
+		case xtime.Hour:
+			_, err = Expire(key, xtime.Day*3)
+			if err != nil {
+				log.Errorf("err:%v", err)
+			}
 		case xtime.Day:
 			_, err = Expire(key, xtime.Week)
+			if err != nil {
+				log.Errorf("err:%v", err)
+			}
+		case xtime.Month:
+			_, err = Expire(key, xtime.Month+xtime.Week)
 			if err != nil {
 				log.Errorf("err:%v", err)
 			}
