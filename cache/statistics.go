4158b0748f936e5a42891aee4e22f865abd72885
Luoxin <luoxin.ttt@gmail.com>
2022-10-25 14:00:00 +0800

    fix

--- cache/statistics.go
+++ cache/statistics.go
@@ -8,15 +8,34 @@ import (
 	"github.com/darabuchi/utils/xtime"
 )
 
-func Statistics(key string, duration time.Duration) {
+func Statistics(key string, durations ...time.Duration) {
+	StatisticsIncr(key, durations...)
+}
+
+func getStatisticsKey(key string, duration time.Duration, t time.Time) string {
 	switch duration {
 	case xtime.Hour:
-		key = fmt.Sprintf("statistics:%s:hour:%s", key, time.Now().Format("2006010215"))
+		return fmt.Sprintf("statistics:%s:hour:%s", key, time.Now().Format("2006010215"))
 	case xtime.Day:
-		key = fmt.Sprintf("statistics:%s:day:%s", key, time.Now().Format("20060102"))
+		return fmt.Sprintf("statistics:%s:day:%s", key, time.Now().Format("20060102"))
+	case xtime.Week:
+		year, week := t.ISOWeek()
+		return fmt.Sprintf("statistics:%s:week:%04d%02d", key, year, week)
 	case xtime.Month:
-		key = fmt.Sprintf("statistics:%s:month:%s", key, time.Now().Format("200601"))
+		return fmt.Sprintf("statistics:%s:month:%s", key, time.Now().Format("200601"))
+	default:
+		return key
 	}
+}
+
+func StatisticsIncr(key string, durations ...time.Duration) {
+	for _, duration := range durations {
+		statisticsIncrBy(key, duration)
+	}
+}
+
+func statisticsIncrBy(key string, duration time.Duration) {
+	key = getStatisticsKey(key, duration, time.Now())
 
 	cnt, err := Incr(key)
 	if err != nil {
@@ -41,19 +60,17 @@ func Statistics(key string, duration time.Duration) {
 			if err != nil {
 				log.Errorf("err:%v", err)
 			}
+		default:
+			_, err = Expire(key, duration+xtime.Week)
+			if err != nil {
+				log.Errorf("err:%v", err)
+			}
 		}
 	}
 }
 
 func GetStatistics(key string, duration time.Duration, t time.Time) int64 {
-	switch duration {
-	case xtime.Hour:
-		key = fmt.Sprintf("statistics:%s:hour:%s", key, t.Format("2006010215"))
-	case xtime.Day:
-		key = fmt.Sprintf("statistics:%s:day:%s", key, t.Format("20060102"))
-	case xtime.Month:
-		key = fmt.Sprintf("statistics:%s:month:%s", key, t.Format("200601"))
-	}
+	key = getStatisticsKey(key, duration, t)
 
 	cnt, err := GetInt64(key)
 	if err != nil {
