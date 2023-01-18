4948a49eaa7c17c4f5a9a5f237bc120e7a1b962d
落心 <luoxin.ttt@gmial.com>
2023-01-18 11:06:36 +0800

    fix

--- file.go
+++ file.go
@@ -34,13 +34,20 @@ func IsFile(path string) bool {
 	return !s.IsDir()
 }
 
-func CopyFile(from, to string) error {
-	input, err := ioutil.ReadFile(from)
+func CopyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
 	if err != nil {
 		return err
 	}
+	defer srcFile.Close()
 
-	err = ioutil.WriteFile(to, input, 0777)
+	dstFile, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
 	if err != nil {
 		return err
 	}
